ac44dcd5cc4a0ffaeb6650089d411f8d0c6db2a5
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-11-10 14:17:05 -0800

    correction to Models&Views Task 1

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -10,12 +10,20 @@ def book_list(request):
 #class based view
 #Create a class-based view in relationship_app/views.py that displays details for a specific library,listing all books available in the library.
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from .models import Library
 
 #Utilize Django's ListView or DetailView to structure this class-based view.
+"""
 class BookListView(ListView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = books
+"""
 
+#Utilize Django's ListView or DetailView to structure this class-based view.
+class BookListView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = books
 
