042b24f754f539877cc299205930958bda887e9b
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-11-10 13:49:09 -0800

    correction to Models&Views Task 1

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+#function based view
 def book_list(request):
     books = Book.objects.all()
     context = {'book_list': books}
-    return render(request,'list_books.html', context)
+    return render(request,'relationship_app/list_books.html', context)
 
-
+#class based view
 from django.views.generic import ListView
 from .models import Book
 
