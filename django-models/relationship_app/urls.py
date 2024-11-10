bcbf0cd8f9bf88a07c213be4f525d7690842a694
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-11-10 14:28:14 -0800

    correction to Models&Views Task 1

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,9 +3,10 @@
 #import necessary packages
 from django.urls import path
 from .views import BookListView
-
+from .views import list_books, LibraryDetailView
 #create url patterns
 urlpatterns = [
-    path('book_list/', views.book_list, name='book_list'),
-    path('books/', BookListView.as_view(), name='book-list'),
+    path('list_books/', views.list_books, name='list-book'),
+    path('book_list/', views.BookListView.as_view(), name = 'book_list')
+    path('books/',LibraryDetailView.as_view(), name='book-list'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 #function based view
-def book_list(request):
+def list_books(request):
     books = Book.objects.all()
     context = {'book_list': books}
     return render(request,'relationship_app/list_books.html', context)
@@ -14,15 +14,14 @@ from django.views.generic.detail import DetailView
 from .models import Library
 
 #Utilize Django's ListView or DetailView to structure this class-based view.
-"""
+
 class BookListView(ListView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = books
-"""
 
 #Utilize Django's ListView or DetailView to structure this class-based view.
-class BookListView(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = books
