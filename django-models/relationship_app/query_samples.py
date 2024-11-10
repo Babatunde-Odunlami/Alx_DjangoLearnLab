dd81af489148165e0703d21903b801d86f62b4b5
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-11-10 12:36:06 -0800

    correction to Models&Views Task 0

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,7 +3,9 @@ from relationship_app.models import Author, Book, Libary, Librarian
 books = Book.objects.select_related('author')
 
 #list all books in a library
+library = Libary.objects.get(name='library_name')
 books = Book.objects.prefetch_related('library')
+books.all()
 
 #retrieve the librarian for a library
 librarian = Librarian.objects.get(library="library")
