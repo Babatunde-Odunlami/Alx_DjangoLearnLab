a88d5f453e94bde1c88ecc612fcb23b109c78df2
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-11-10 15:07:13 -0800

    Models&Views Task 2

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -32,7 +32,7 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
+from django.contrib.auth import login
 # Login view
 class Login(auth_views.LoginView):
     template_name = 'relationship_app/login.html'
