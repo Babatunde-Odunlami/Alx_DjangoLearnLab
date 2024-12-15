523fc14e15a0f507119c3b25b2b9f77f67a2d680
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-12-15 08:40:20 -0800

    Week 14 Task 1

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . import views .   #register and profile views
+from . import views    #register and profile views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-        path('login/', LoginView.as_view(template_name = 'blog/login.html'), name = 'login'),
-        path('logout/', LogoutView.as_view(template_name = 'blog/logout.html'), name= 'logout'),
-        path('register/', views.register.as_view(), name = 'register'),
-        path('profile/', views.ProfileView.as_view(), name = 'profile'),
+        path('login/', LoginView.as_view(template_name = 'blog/login.html'), name = 'Login'),
+        path('logout/', LogoutView.as_view(template_name = 'blog/logout.html'), name= 'Logout'),
+        path('register/', views.register.as_view(), name = 'Register'),
+        path('profile/', views.ProfileView.as_view(), name = 'Profile'),
+        path('update_profile/', views.UpdateProfileView.as_view(), name = 'Update'),
         ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -12,7 +12,22 @@ class register(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'blog/register.html'
 
-
 #profile view
-class ProfileView(TemplateView):
+class ProfileView(TemplateView):i
     template_name = 'blog/profile.html'
+
+
+#Profile management: create a view that allows authenticated users to view and edit thier profiles.
+#This view should handle POST requests to update user information.
+#extend the user model to enable addition of  bio and profile pic
+
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class UpdateProfileView(UpdateView, LoginReguiredMixin):
+    model = User
+    template_name = 'blog/update_profile.html'
+    success_url = reverse_lazy('profile') # where to redirect after successful update
+
+    def get_object(self):
+        return self.request.user    #ensure user can only edit thier own profile
